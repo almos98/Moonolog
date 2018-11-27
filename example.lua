5a3767604f7c4d50c11707c8e535392f2310a622
Alexion Ramos <ramosa@tcd.ie>
2018-11-27 01:04:24 +0000

    Added example file.

---
+++ example.lua
@@ -0,0 +1,16 @@
+local log = require 'log'
+
+function printT(t)
+    for i, v in next, t do
+        print(i,v)
+    end
+end
+
+log:Trace("a trace message")
+log:Debug("a debug message")
+log:Info("an info message")
+log:Warn("a warning")
+log:Error("an error")
+log:Fatal("a fatal error")
+
+log:Tracef("this is a %sing test", "laugh")
